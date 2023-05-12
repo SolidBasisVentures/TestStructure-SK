13ada38b6719d5a32169e8c414b9e58b8d387594
Dennis Peters <denjpeters@gmail.com>
2023-05-12 08:34:16 -0400

    Explain raw, promise, and deferred promise

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,18 +2,24 @@ import type {LayoutServerLoad} from './$types';
 import {BackendAdd} from "$lib/server/backend";
 
 export const load = (async ({depends}) => {
-	async function timeDelay(ms: number) {
-		return new Promise<string>(resolve => {
-			setTimeout(() => {
-				resolve(new Date().toISOString());
-			}, ms);
-		});
-	}
-
 	const result = {
 		layoutServerNumber: BackendAdd(1),
-		layoutServerDT: timeDelay(20),
+		// Raw Data: These three lines will run in series, and take 6ms
+		awaitLayoutServerDT: await timeDelay(2),
+		awaitLayoutServerDT2: await timeDelay(1),
+		awaitLayoutServerDT3: await timeDelay(3),
+		// Promises: These three lines will run in parallel, and take 3ms,
+		// but SvelteKit will resolve the promises before the page is loaded
+		// so that the page will see the final data type
+		// The page WILL NOT load before all of these promises are resolved
+		layoutServerDT: timeDelay(2),
+		layoutServerDT2: timeDelay(1),
+		layoutServerDT3: timeDelay(3),
 		toOverwrite: 'Top Level',
+		// Deferred Promises: Everything that is in a sub-property is a deferred promise
+		// The following properties will return to the layout or page as promises, not the resolved value
+		// The layout or page will need to resolve these promises before displaying data
+		// The page will load before these promises are resolved
 		lazy: {
 			layoutServerDTDelay: timeDelay(2000),
 			layoutServerDTNoDelay: new Promise<string>(resolve => {resolve(new Date().toISOString());})
@@ -26,3 +32,11 @@ export const load = (async ({depends}) => {
 
 	return result;
 }) satisfies LayoutServerLoad;
+
+async function timeDelay(ms: number) {
+	return new Promise<string>((resolve) => {
+		setTimeout(() => {
+			resolve(new Date().toISOString());
+		}, ms);
+	});
+}
--- src/routes/Sub[sub_id=integer]/+page.server.ts
+++ src/routes/Sub[sub_id=integer]/+page.server.ts
@@ -1,24 +1,18 @@
-import type {PageServerLoadEvent} from './$types'
+import type {PageServerLoad} from './$types'
 import {BackendAdd} from '$lib/server/backend'
 import type {Actions} from '@sveltejs/kit'
 
-export async function load(e: PageServerLoadEvent) {
-	const parentData = await e.parent()
-
-	const layoutServerDTDelay = await parentData.lazy.layoutServerDTDelay
-
-	// console.log('layoutServerDTDelay', layoutServerDTDelay)
-
+export const load = (async ({params}) => {
 	return {
-		subPageServerNumber: BackendAdd(+e.params.sub_id),
+		subPageServerNumber: BackendAdd(+params.sub_id),
 		subPageTestData: 1,
 		toOverwrite: 'Sub',
 	}
-}
+}) satisfies PageServerLoad
 
 export const actions: Actions = {
 	testAction: async (e) => {
-		// console.log('testAction FormData', await e.request.formData())
+		console.log('testAction FormData', await e.request.formData())
 
 		return {success: true, message: 'Test Executed'}
 	}
